d67d20b8248230ffc97c1c750dd68b84ade389f9
Vinícius Figueiroa <vinissfigueiroa@gmail.com>
2022-09-16 15:51:12 -0300

    feat(server): create base structure of routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,29 +2,24 @@ import express from "express";
 
 const app = express();
 
-app.get("/ads", (req, res) => {
-  return res.json([
-    {
-      id: 1,
-      name: "Anúncio 1",
-    },
-    {
-      id: 2,
-      name: "Anúncio 2",
-    },
-    {
-      id: 3,
-      name: "Anúncio 3",
-    },
-    {
-      id: 4,
-      name: "Anúncio 4",
-    },
-    {
-      id: 5,
-      name: "Anúncio 5",
-    },
-  ]);
+app.get("/games", (req, res) => {
+  return res.json([]);
+});
+
+app.post("/ads", (req, res) => {
+  return res.status(201).json([]);
+});
+
+app.get("/games/:id/ads", (req, res) => {
+  // const gameId = req.params.id;
+
+  return res.json([]);
+});
+
+app.get("/ads/:id/discord", (req, res) => {
+  // const adId = req.params.id;
+
+  return res.json([]);
 });
 
 app.listen(3333);

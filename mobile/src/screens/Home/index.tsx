5edacaf56fbd20945dcb9071ae1e447f3395ec7b
Vinícius Figueiroa <vinissfigueiroa@gmail.com>
2022-09-15 23:28:36 -0300

    feat(mobile): add heading and game cards to home screen

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,14 +1,36 @@
 import React from 'react';
-import { Image, View } from 'react-native';
+import { FlatList, Image, ScrollView, View } from 'react-native';
 
 import logoImg from '../../assets/logo.png';
+
+import { GameCard } from '../../components/GameCard';
+import { Heading } from '../../components/Heading';
+
+import { GAMES } from '../../utils/games';
 
 import { styles } from './styles';
 
 export function Home() {
   return (
-    <View style={styles.container}>
+    <ScrollView
+      style={styles.container}
+      contentContainerStyle={{ alignItems: 'center' }}
+    >
       <Image style={styles.logo} source={logoImg} />
-    </View>
+
+      <Heading
+        title="Encontre seu duo"
+        subtitle="Selecione o game que deseja jogar..."
+      />
+
+      <FlatList
+        data={GAMES}
+        keyExtractor={item => item.id}
+        renderItem={({ item }) => <GameCard data={item} />}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.listContent}
+      />
+    </ScrollView>
   );
 }
--- mobile/src/screens/Home/styles.ts
+++ mobile/src/screens/Home/styles.ts
@@ -3,12 +3,15 @@ import { StyleSheet } from 'react-native';
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
   },
   logo: {
     width: 214,
     height: 120,
     marginTop: 74,
     marginBottom: 48,
+  },
+  listContent: {
+    paddingLeft: 32,
+    paddingRight: 64,
   },
 });

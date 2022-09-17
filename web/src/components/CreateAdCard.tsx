105e2fe453212dab1fd2e656b4337670aa8b6803
Vinícius Figueiroa <vinissfigueiroa@gmail.com>
2022-09-16 23:20:15 -0300

    feat(web): change button of create ad card component

--- web/src/components/CreateAdCard.tsx
+++ web/src/components/CreateAdCard.tsx
@@ -1,4 +1,5 @@
 import { MagnifyingGlassPlus } from 'phosphor-react';
+import * as Dialog from '@radix-ui/react-dialog';
 
 export function CreateAdCard() {
   return (
@@ -13,10 +14,10 @@ export function CreateAdCard() {
           </span>
         </div>
 
-        <button className="py-3 px-4 bg-violet-500 text-white rounded hover:bg-violet-600 flex items-center gap-3">
+        <Dialog.Trigger className="py-3 px-4 bg-violet-500 text-white rounded hover:bg-violet-600 flex items-center gap-3">
           <MagnifyingGlassPlus size={24} />
           Publicar anúncio
-        </button>
+        </Dialog.Trigger>
       </div>
     </div>
   );

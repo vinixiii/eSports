74d72f0eb5755b257b2baec4bec7786ae9b642db
Vinícius Figueiroa <vinissfigueiroa@gmail.com>
2022-09-17 12:38:58 -0300

    feat(mobile): create game screen ads list

--- mobile/src/screens/GameScreen/index.tsx
+++ mobile/src/screens/GameScreen/index.tsx
@@ -1,19 +1,23 @@
-import { Image, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { FlatList, Image, ScrollView, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { RouteProp, useRoute, useNavigation } from '@react-navigation/native';
 import { Entypo } from '@expo/vector-icons';
 
+import { AppParamList } from '../../@types/navigation';
+
 import { Background } from '../../components/Background';
+import { CustomTouchableOpacity } from '../../components/CustomTouchableOpacity';
+import { Heading } from '../../components/Heading';
+import { Ad, DuoCard } from './components/DuoCard';
 
 import logoImg from '../../assets/logo.png';
-
-import { AppParamList } from '../../@types/navigation';
 
 import { THEME } from '../../theme';
 
 import { styles } from './styles';
-import { Heading } from '../../components/Heading';
-import { CustomTouchableOpacity } from '../../components/CustomTouchableOpacity';
+
+const IP = process.env.IP;
 
 type GameScreenRouteProp = RouteProp<AppParamList, 'GameScreen'>;
 
@@ -21,35 +25,72 @@ export function GameScreen() {
   const { params } = useRoute<GameScreenRouteProp>();
   const { goBack } = useNavigation();
 
+  const [ads, setAds] = useState<Ad[]>([]);
+
   const handleGoBack = () => {
     goBack();
   };
 
+  useEffect(() => {
+    fetch(`http://${IP}/games/${params.id}/ads`)
+      .then(res => res.json())
+      .then(data => {
+        setAds(data);
+      });
+  }, []);
+
   return (
     <Background>
       <SafeAreaView style={styles.container}>
-        <View style={styles.header}>
-          <CustomTouchableOpacity
-            style={styles.backButton}
-            onPress={handleGoBack}
-          >
-            <Entypo
-              name="chevron-thin-left"
-              size={24}
-              color={THEME.COLORS.CAPTION_300}
-            />
-          </CustomTouchableOpacity>
+        <ScrollView
+          contentContainerStyle={styles.content}
+          showsVerticalScrollIndicator={false}
+        >
+          <View style={styles.header}>
+            <CustomTouchableOpacity
+              style={styles.backButton}
+              onPress={handleGoBack}
+            >
+              <Entypo
+                name="chevron-thin-left"
+                size={24}
+                color={THEME.COLORS.CAPTION_300}
+              />
+            </CustomTouchableOpacity>
 
-          <Image style={styles.logo} source={logoImg} />
-        </View>
+            <Image style={styles.logo} source={logoImg} />
+          </View>
 
-        <Image
-          style={styles.banner}
-          source={{ uri: params.bannerUrl }}
-          resizeMode="cover"
-        />
+          <Image
+            style={styles.banner}
+            source={{ uri: params.bannerUrl }}
+            resizeMode="cover"
+          />
 
-        <Heading title={params.title} subtitle="Conecte-se e comece a jogar!" />
+          <Heading
+            title={params.title}
+            subtitle="Conecte-se e comece a jogar!"
+          />
+
+          <FlatList
+            style={styles.listContainer}
+            contentContainerStyle={
+              ads.length > 1 ? styles.listContent : styles.emptyListContent
+            }
+            data={ads}
+            keyExtractor={item => item.id}
+            renderItem={({ item }) => (
+              <DuoCard data={item} onConnect={() => null} />
+            )}
+            ListEmptyComponent={() => (
+              <Text style={styles.emptyListText}>
+                Ainda não há anúncios para esse game :(
+              </Text>
+            )}
+            horizontal
+            showsHorizontalScrollIndicator={false}
+          />
+        </ScrollView>
       </SafeAreaView>
     </Background>
   );
--- mobile/src/screens/GameScreen/styles.ts
+++ mobile/src/screens/GameScreen/styles.ts
@@ -1,9 +1,14 @@
 import { StyleSheet } from 'react-native';
+import { THEME } from '../../theme';
 
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  content: {
+    flexGrow: 1,
     alignItems: 'center',
+    paddingBottom: 28,
   },
   header: {
     width: '100%',
@@ -23,8 +28,27 @@ export const styles = StyleSheet.create({
   },
   banner: {
     marginTop: 32,
+    marginBottom: 24,
     width: 311,
     height: 160,
     borderRadius: 8,
+  },
+  listContainer: {
+    width: '100%',
+    marginTop: 16,
+  },
+  listContent: {
+    paddingLeft: 32,
+    paddingRight: 64,
+  },
+  emptyListContent: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  emptyListText: {
+    color: THEME.COLORS.CAPTION_400,
+    fontSize: THEME.FONT_SIZE.SM,
+    fontFamily: THEME.FONT_FAMILY.REGULAR,
   },
 });
